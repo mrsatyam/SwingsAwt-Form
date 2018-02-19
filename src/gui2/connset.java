04b08d8551ee56e263c8c3ed4b4e142205e24879
mrsatyam <mrsatyamjoshi@gmail.com>
2018-02-19 11:39:03 +0530

    first commit

---
+++ src/gui2/connset.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui2;
+
+/**
+ *
+ * @author hpm
+ */
+public class connset {
+    static String driver="com.mysql.jdbc.Driver";
+    static String connstr="jdbc:mysql://localhost:3306/coerlogin";
+    
+}
